12bea6d57c49030a053572dc0eae8445e34a6336
saymonsh <141330014+saymonsh@users.noreply.github.com>
2026-01-20 13:41:02 +0200

    refactor: move main script logic into try block and remove duplicate option line in `add_candidats.py`.

--- login.py
+++ login.py
@@ -53,74 +53,72 @@ def login_and_process(uploaded_file_path, ui):
         driver.implicitly_wait(30)
         driver.maximize_window()
 
-    username = driver.find_element(By.XPATH, "//input[@id='username']")
-    username.send_keys(parm.USER_NAME)
-    password = driver.find_element(By.XPATH, "//input[@id='password']")
-    password.send_keys(parm.PASSWORD)
-    driver.find_element(By.XPATH, "//input[@id='Login']").click()
-    tc = driver.find_element(By.XPATH, "//input[@id='tc']")
-    tc.send_keys(totp.now())
-    driver.find_element(By.XPATH, "//input[@id='save']").click()
+        username = driver.find_element(By.XPATH, "//input[@id='username']")
+        username.send_keys(parm.USER_NAME)
+        password = driver.find_element(By.XPATH, "//input[@id='password']")
+        password.send_keys(parm.PASSWORD)
+        driver.find_element(By.XPATH, "//input[@id='Login']").click()
+        tc = driver.find_element(By.XPATH, "//input[@id='tc']")
+        tc.send_keys(totp.now())
+        driver.find_element(By.XPATH, "//input[@id='save']").click()
 
-    counter = 1
+        counter = 1
 
-    print(f"Total rows in Excel: {len(excel_data)}")
+        print(f"Total rows in Excel: {len(excel_data)}")
 
-    for index, row in excel_data.iterrows():
-        id_number = row['תעודות זהות']
-        typer = row['סוג']
-        date = row['תאריך']
-        
-        # חישוב אחוזים
-        percent = int((counter / len(excel_data)) * 100)
-        print(f"{counter}/{len(excel_data)} - {percent}%")
-        ui["Progressbar"].value = percent
-        
-        if row['סוג'] == 1:  # יצירת הכנת תוכנית אישית + פעילות ודיווח שירות
-            try:
-                perform_search(driver, id_number)
-                create_actions(driver, typer)
-                create_report(driver, date, typer)
-            except Exception as e:
-                print(f"תקלה במספר זהות: {id_number}, {str(e)}")
-                exit(400)
-        elif row['סוג'] == 2:  # יצירת הכנת תוכנית אישית + פעילות ללא דיווח
-            try:
-                perform_search(driver, id_number)
-                create_actions(driver, typer)
-            except Exception as e:
-                print(f"תקלה במספר זהות: {id_number}, {str(e)}")
-        elif row['סוג'] == 3:  # דיווח שירות תוכנית אישית בלבד
-            try:
-                perform_search(driver, id_number)
-                create_report(driver, date, typer)
-            except Exception as e:
-                print(f"תקלה במספר זהות: {id_number}, {str(e)}")
-        elif row['סוג'] == 4:  # יצירת פעילות ודיווח שירות ללא יצירת תוכנית אישית
-            try:
-                perform_search(driver, id_number)
-                create_actions(driver, typer)
-                create_report(driver, date, typer)
+        for index, row in excel_data.iterrows():
+            id_number = row['תעודות זהות']
+            typer = row['סוג']
+            date = row['תאריך']
+            
+            # חישוב אחוזים
+            percent = int((counter / len(excel_data)) * 100)
+            print(f"{counter}/{len(excel_data)} - {percent}%")
+            ui["Progressbar"].value = percent
+            
+            if row['סוג'] == 1:  # יצירת הכנת תוכנית אישית + פעילות ודיווח שירות
+                try:
+                    perform_search(driver, id_number)
+                    create_actions(driver, typer)
+                    create_report(driver, date, typer)
+                except Exception as e:
+                    print(f"תקלה במספר זהות: {id_number}, {str(e)}")
+                    exit(400)
+            elif row['סוג'] == 2:  # יצירת הכנת תוכנית אישית + פעילות ללא דיווח
+                try:
+                    perform_search(driver, id_number)
+                    create_actions(driver, typer)
+                except Exception as e:
+                    print(f"תקלה במספר זהות: {id_number}, {str(e)}")
+            elif row['סוג'] == 3:  # דיווח שירות תוכנית אישית בלבד
+                try:
+                    perform_search(driver, id_number)
+                    create_report(driver, date, typer)
+                except Exception as e:
+                    print(f"תקלה במספר זהות: {id_number}, {str(e)}")
+            elif row['סוג'] == 4:  # יצירת פעילות ודיווח שירות ללא יצירת תוכנית אישית
+                try:
+                    perform_search(driver, id_number)
+                    create_actions(driver, typer)
+                    create_report(driver, date, typer)
 
-            except Exception as e:
-                print(f" תקלה במספר זהות: {id_number}")
-        elif row['סוג'] == 5:  # יצירת פעילות ללא תוכנית אישית ודיווח שירות
-            try:
-                perform_search(driver, id_number)
-                create_actions(driver, typer)
+                except Exception as e:
+                    print(f" תקלה במספר זהות: {id_number}")
+            elif row['סוג'] == 5:  # יצירת פעילות ללא תוכנית אישית ודיווח שירות
+                try:
+                    perform_search(driver, id_number)
+                    create_actions(driver, typer)
 
-            except Exception as e:
-                print(f" תקלה במספר זהות: {id_number}")
-        elif row['סוג'] == 6:  # דיווח שירות על פעילות אחרת
-            try:
-                perform_search(driver, id_number)
-                create_report(driver, date, typer)
-            except Exception as e:
-                print(f" תקלה במספר זהות: {id_number} - {e}")
+                except Exception as e:
+                    print(f" תקלה במספר זהות: {id_number}")
+            elif row['סוג'] == 6:  # דיווח שירות על פעילות אחרת
+                try:
+                    perform_search(driver, id_number)
+                    create_report(driver, date, typer)
+                except Exception as e:
+                    print(f" תקלה במספר זהות: {id_number} - {e}")
 
-        counter += 1
-
-        counter += 1
+            counter += 1
 
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
